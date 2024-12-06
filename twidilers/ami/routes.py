869f07d37c7fd4db8c42e0af3aeb326f0f51fbfe
hihihioli <olivermandel123@gmail.com>
2024-12-06 12:06:35 -0500

    fixed ami's template folder

--- twidilers/ami/routes.py
+++ twidilers/ami/routes.py
@@ -1,18 +1,18 @@
 from flask import current_app, render_template, abort, request, redirect, url_for, flash, session,jsonify
 import sqlalchemy
-from . import blue as app #Blueprint imported as app so blueprint layer 
+from . import ami as app #Blueprint imported as app so blueprint layer 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('ami/index.html')
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('ami/login.html')
 
 @app.route('/signup',methods=["GET","POST"])
 def signup():
     if request.method == 'GET':
-        return render_template('signup.html')
+        return render_template('ami/signup.html')
     if request.method == "POST":
         pass
