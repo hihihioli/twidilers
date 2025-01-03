8b7cb23b43cbce43b563dac2bd537c157c740e45
hihihioli <olivermandel123@gmail.com>
2025-01-03 15:41:40 -0500

    Refactor settings handling

--- twidilers/functions.py
+++ twidilers/functions.py
@@ -1,6 +1,8 @@
 from .models import Account,db
-from flask import session
+from flask import session,flash
 import re #for regular expressions
+from io import BytesIO
+from PIL import Image, ImageOps, UnidentifiedImageError
 
 def save():
     try:
@@ -24,3 +26,57 @@ def checkUsername(input):
         return True
     else:
         return False
+
+def deleteAccount():
+    account = findAccount()
+    db.session.delete(account)
+    db.session.commit()
+    flash('Successfully Deleted Account','success')
+
+def newPassword(request):
+    account = findAccount()
+    new_password = request.form.get('new-password')
+    old_password = request.form.get('old-password')
+    username = account.username
+    if not account.check_password(old_password): #Checks if the old password is correct
+        flash('Current password is incorrect','error')
+    elif new_password == username: # checks if the new password is the same as the username
+        flash('Password cannot be the same as the username','error')
+    elif len(new_password) < 8:
+        flash('Password must be at least 8 characters long','error')
+    elif new_password == old_password: #Checks if the new password is the same as the old password
+        flash('New password cannot be the same as the old password','error')
+    else:
+        account.password = new_password
+        db.session.commit()
+        flash('Password changed successfully','success')
+
+def changeDisplay(request):
+    account = findAccount()
+    new_name = request.form.get('change-name')
+    account.displayname = new_name
+    db.session.commit()
+    flash(f'Display Name Changed to {account.displayname}','success')
+
+def changePFP(request):
+    account = findAccount()
+    try:
+        img = Image.open(request.files['file'])
+        img = ImageOps.fit(img,(200,200)) #sets the file resolution
+        temp_file = BytesIO()
+        img.save(temp_file, format="PNG")
+        account.photo = temp_file.getvalue()
+        db.session.commit()
+        flash('Updated Photo Successfully','success')
+    except UnidentifiedImageError:
+        flash('Unsupported Image Type','error')
+    except Exception as e:
+        flash(f'An Error Occured: {e}')
+
+def changeBio(request):
+    account = findAccount()
+    new_userdata = account.userdata.copy()  # Create a copy of the existing userdata
+    new_userdata['bio'] = request.form.get('bio')  # Update the bio field
+    account.userdata = new_userdata 
+    db.session.commit()
+    flash('Bio Updated Successfully','success')
