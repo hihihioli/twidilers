6b195944eb0b361430f870bdf70d319ad6699b7f
dereena <dareharebarkan@gmail.com>
2025-01-02 21:22:12 -0500

    clearNotifs and fontawesome.js do not work outside of base.html

--- twidilers/base/static/javascript/fontawesome.js
+++
@@ -1,7 +0,0 @@
-// Adds fontawesome styles to the page once everything has loaded
-window.addEventListener('load', function() {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = "{{ url_for('.static', filename='styles/fontawesome.css') }}";
-    document.body.appendChild(link);
-});
