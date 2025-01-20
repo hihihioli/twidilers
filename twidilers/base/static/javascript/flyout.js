2e5f4c3052599553a1161cd360e2a521dbe5e8a0
wall03 <65470133+wall03@users.noreply.github.com>
2025-01-20 18:26:04 -0500

    filter button quick fix

--- twidilers/base/static/javascript/flyout.js
+++ twidilers/base/static/javascript/flyout.js
@@ -3,20 +3,15 @@
 // This is for the function flyOut
 
 // btn supplies the input button id while mnu supplies the content id
-function flyOut(mnu,btn) {
+function flyOut(mnu) {
     const userMenu = document.getElementById('popupMenu');
-    const button = document.getElementById(btn);
     const menu = document.getElementById(mnu);
 
-    menu.style.display = 'block';
-
-    button.addEventListener('click', () => {
-        if (menu.style.display === 'none') {
-            menu.style.display = 'block';
-            userMenu.style.display = 'none';
-        } else {
-            menu.style.display = 'none';
-        }
-        });  
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block';
+        userMenu.style.display = 'none';
+    } else {
+        menu.style.display = 'none';
+    }
 
 }
