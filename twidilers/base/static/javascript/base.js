8aa1981d93a794c8bc4ac1494ed8a39a0203dc61
wall03 <65470133+wall03@users.noreply.github.com>
2025-08-09 18:50:17 -0400

    Updated settings + styling to current standards

--- twidilers/base/static/javascript/base.js
+++ twidilers/base/static/javascript/base.js
@@ -4,11 +4,14 @@ const userMenu = document.getElementById('popupMenu');
 const notifButton = document.getElementById('navbarbell');
 const notifMenu = document.getElementById('notif-popup');
 const filterMenu = document.getElementById('filter-form');
+const locationthing = document.getElementById('location');
+
 
 
 notifMenu.style.display = 'none';
 userMenu.style.display = 'none';
 
+// Expands notif page if it's clicked
 notifButton.addEventListener('click', () => {
 if (notifMenu.style.display === 'none') {
     notifMenu.style.display = 'flex';
