f3a86461e8441d1dab891a5281466cfd29eddaf4
wall03 <65470133+wall03@users.noreply.github.com>
2025-09-30 15:22:12 -0400

    fixed: app.post will always return full profile page

--- twidilers/base/api.py
+++ twidilers/base/api.py
@@ -356,16 +356,16 @@ def profaction(username):
         post:Post = db.session.execute(db.select(Post).filter_by(id=post_id)).scalar()
         if post.username == username:
             flash('You cannot like your own post','error')
-            return redirect(url_for('.profile',username=username))
+            return profile(username)
         if account.id in [person.id for person in post.liked_by]:
             post.liked_by.remove(account)
             db.session.commit()
             flash('Post Unliked','success')
-            return redirect(url_for('.profile',username=username))
+            return profile(username)
         post.liked_by.append(account)
         db.session.commit()
         flash('Post Liked','success')
-        return redirect(url_for('.profile',username=username))
+        return profile(username)
     if "delete-post" in request.form:
         post_id = request.form.get('delete-post-id')
         post = db.session.execute(db.select(Post).filter_by(id=post_id)).scalar()
@@ -373,11 +373,10 @@ def profaction(username):
             flash('You cannot delete a post that is not yours','error')
         deletePost(post)
         flash('Post Deleted','success')
-        posts = sorted(account.posts, key=lambda c: c.date, reverse=True)
         return profile(username) 
     else:
         flash('A desync error occured','error') #The request type is unknown. This catches all of the invalid requests and allows for further debugging
-        return redirect(url_for('.profile',username=username))
+        return profile(username)
 
 
 @app.get('/verify/<token>')
